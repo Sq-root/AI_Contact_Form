b6c64d8522789d6f08deec7504d35e010d7a1781
gaurav.davda <gauravdavda34@gmail.com>
2026-05-29 13:22:57 +0530

    feat: update registration API endpoint from /api/register to /api/registrations

--- components/sections/RegisterForm/index.tsx
+++ components/sections/RegisterForm/index.tsx
@@ -106,7 +106,7 @@ export default function RegisterForm() {
     setErrors({});
 
     const apiBase = (process.env.NEXT_PUBLIC_API_BASE_URL || "").replace(/\/$/, "");
-    const registerUrl = `${apiBase}/api/register`;
+    const registerUrl = `${apiBase}/api/registrations`;
 
     let res: Response;
     try {
